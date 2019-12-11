0f179127f244f92b57190897a88e831e527877d7
soonjing <39853992+soonjing@users.noreply.github.com>
2019-12-11 21:30:03 +0800

    Footer + Home Page

--- client/src/components/footer.vue
+++ client/src/components/footer.vue
@@ -1,14 +1,44 @@
 <template>
-    
+<footer id="footer">
+    <div class="container">
+        
+      <div class="footer-block">      
+          <img src="..\assets\img\Giant forest Inn Hotel Logo.png"/>    
+          <p class="footerInfo">
+          Address: 7921 South Pin Oak Drive Tracy, CA 95376 <br />
+          Contact-No: 209-200-6070 <br />
+          Email: <a href="mailto:stackoverkill@outlook.com?Subject=Enquiry%20on%20Giant%20Forest%20Inn"> giantForestInn@hotel.com</a>
+          </p>
+      </div>      
+    </div>
+</footer>
 </template>
 
 <script>
 export default {
-    name:'Footer',
-
+    name:'Footer'
 }
 </script>
 
 <style scoped>
+div img{
+    float:left; 
+    width:500px;   
+    padding-right: 150px;
+}
+#footer {    
+    margin-left:auto;
+    margin-right:auto;
+    width: 100%;
+    position: absolute;    
+    background-color: #f5f5f5;
+    padding-top:20px;
+    padding-bottom:40px;
+}
+.footer-block {
+    margin: 20px 0;
+}
+.footerInfo{
+}
 
 </style>
--- client/src/components/home.vue
+++ client/src/components/home.vue
@@ -1,11 +1,14 @@
 <template>
-<section>
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      Welcome!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+<section id="home">
+    <header class="text-white">
+      <div class="container text-center">
+        <h1>Giant Forest Inn</h1>
+        <p class="lead"><b>Luxury and Private </b></p>        
+        <br /> <br />
+        <a class="btn btn-primary waves-effect border-0 btn-lg" href="/booking">Book now</a>        
+        <a class="btn btn-white waves-effect btn-lg border-white text-white" href="/room">Read more</a>
+      </div>
+    </header>
 </section>
 </template>
 
@@ -21,5 +24,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.container a{
+  margin-left:20px;
+  margin-right:20px;
+}
+#home{
+  padding: 156px 0 100px;
+  height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('../assets/img/mossyforest.jpg');
+  /* background-image: url(../assets/img/mossyforest.jpg); */
+  background-size: cover;
+  background-repeat:no-repeat;
+}
 </style>
